23cb4a59243169b9cad946efdd8892e4a47a118e
hacknife <hacknife@outlook.com>
2021-12-22 17:00:15 +0800

    rename package

---
+++ example/src/main/java/com/iwdael/coolprogress/example/LineActivity.java
@@ -0,0 +1,20 @@
+package com.iwdael.coolprogress.example;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * author  : Black Chopper
+ * e-mail  : 4884280@qq.com
+ * github  : http://github.com/BlackChopper
+ * project : CustomProgress
+ */
+public class LineActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_line);
+
+
+    }
+}
